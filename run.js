3cce07c702cdb7d2f460ef590b3d5d86149dc797
Turner Bohlen <turnerbohlen@gmail.com>
2012-04-23 04:38:54 -0400

    Began visualization implementation with kineticJS

--- run.js
+++ run.js
@@ -6,13 +6,13 @@ Date: April 20th, 2012
 
 Run executes the program, beginning the stepping loop.
 */
-
+console.log(ITesting.runTests().toString());
 var earth = new IEarth();
-earth.init()
+earth.init();
 var animal = new IBody(earth);
 earth.registerInputAction(animal.gatherInput);
 earth.registerProcessAction(animal.processInput);
 var visualizer = new IVisualizer(earth);
+visualizer.animals.push(animal);
 earth.preStepActions.push(visualizer.draw);
-
 IEarth.start();
--- visualizer.js
+++ visualizer.js
@@ -1,13 +1,68 @@
-function draw() {  
-    var canvas = document.getElementById("canvas");  
-    var ctx = canvas.getContext("2d");
-}
-
 function IVisualizer(earth) {
-    this.canvas = document.getElementById("canvas");  
-    this.ctx = canvas.getContext("2d");
+    this.earth = earth;
+    this.animals = [];
+    this.stage = new Kinetic.Stage({
+        container: "canvas",
+        width: earth.board.sideLength*10,
+        height: earth.board.sideLength*10
+    });
+    this.bg = new Kinetic.Layer();
+    var bgRect = new Kinetic.Rect({
+        x: 0
+        , y: 0
+        , width: earth.board.sideLength*10
+        , height: earth.board.sideLength*10
+        , fill: "#BBBBBB"
+        , strokeWidth: 0
+    });
+    this.bg.add(bgRect);
+    this.stage.add(this.bg);
 }
 
 IVisualizer.prototype.draw = function () {
-    
-}
+    this.clearScreen();
+    this.drawWorld();
+    this.drawAnimals();
+};
+
+IVisualizer.prototype.clearScreen = function () {
+    this.stage.clear();
+    this.stage.add(this.bg);
+};
+
+IVisualizer.prototype.drawWorld = function () {
+    var layer = new Kinect.Layer();
+    for(var i = 0; i < this.earth.board.sideLength; i++) {
+        for(var j = 0; j < this.earth.board.sideLength; j++) {
+            var color = (this.earth.board.getLocale([i, j]).contents == "food") ? "#AA2222": "#222222";
+            var rect = new Kinect.Rect({
+                x: i*10
+                , y: j*10
+                , width: 10
+                , height: 10
+                , fill: color
+                , stroke: "#000000"
+                , strokeWidth: 1
+            });
+            layer.add(rect);
+        }
+    }
+    this.stage.add(layer);
+};
+
+IVisualizer.prototype.drawAnimals = function() {
+    var layer = new Kinect.Layer();
+    for(var i = 0; i < this.animals.length; i++) {
+        var animal = this.animals[i];
+        var rect = new Kinect.Rect({
+            x: (animal.loc[0]*10)+2
+            , y: (animal.loc[1]*10)+2
+            , width: 6
+            , height: 6
+            , fill: "#2222AA"
+            , strokeWidth: 0
+        });
+        layer.add(rect);
+    }
+    this.stage.add(rect);
+};
